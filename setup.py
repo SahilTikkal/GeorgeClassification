4d3d728f896efcdaaa80f88ba1744787c5ae97c5
SahilTikkal <tikkalsahil@gmail.com>
2023-08-29 18:18:41 +0530

    Initialcommit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying if the image contains St. George or not.',
+    author='Sahil_Tikkal',
+    license='',
+)
